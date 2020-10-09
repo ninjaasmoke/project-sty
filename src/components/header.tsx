a3c1084bf5c65b03533cb3035bacc06d4bc93578
Nithin Sai K J <nithins674@gmail.com>
2020-10-09 20:42:44 +0530

    added header

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function headerComp() {
+    return (
+        <div>
+            <header className="nav">
+                <div className="greeting">
+                    Hey there!
+                </div>
+            </header>
+        </div>
+    )
+}
---
+++ src/components/mainComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeaderComp from './header';
+
+export default function mainComponent() {
+  return (
+    <div>
+      <HeaderComp></HeaderComp>
+    </div>
+  )
+}
+
